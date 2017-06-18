e206ec7e88a890d0f1863fefe844962f4a125d12
viable-hartman <viable.hartman@gmail.com>
2017-06-18 01:33:26 -0700

    Moving description to more appropriate text column

--- database.sql
+++ database.sql
@@ -42,27 +42,11 @@ CREATE TABLE `pages` (
   `source` varchar(2048) DEFAULT NULL,
   `link` varchar(2048) DEFAULT NULL,
   `statusCode` int(11) DEFAULT NULL,
-  `description` varchar(2048) DEFAULT NULL,
+  `description` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38768 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=76700 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `pages_tmp`
---
-
-DROP TABLE IF EXISTS `pages_tmp`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pages_tmp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `source` varchar(2048) DEFAULT NULL,
-  `link` varchar(2048) DEFAULT NULL,
-  `statusCode` int(11) DEFAULT NULL,
-  `description` varchar(2048) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -73,4 +57,4 @@ CREATE TABLE `pages_tmp` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-16 13:24:05
+-- Dump completed on 2017-06-18  1:32:19
